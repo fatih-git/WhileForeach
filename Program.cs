850d52f8681bd0c3091051a45600fcf38d420dcc
fatih-git <72505641+fatih-git@users.noreply.github.com>
2022-10-28 15:05:43 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace WhileForeach
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            double girilen = 0;
+            Console.WriteLine("Sayı Giriniz");
+            girilen=Convert.ToInt32(Console.ReadLine());
+
+            int count = 1;
+            double toplam = 0;
+
+            while (count <= girilen)
+            {
+                toplam += count;
+                count++;
+            }
+
+            Console.WriteLine(toplam / girilen);
+
+            string[] arabalar = { "BMW", "Mercedes", "Volkwagen" };
+
+            foreach (var araba in arabalar)  //"var" tipi belli olmayan genel bir tanımlama ifadesidir. 
+            {
+                Console.WriteLine(araba);
+            }
+
+        }
+
+    }
+
+}
